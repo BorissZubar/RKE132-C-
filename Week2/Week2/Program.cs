05630c064025718adc1151496706df8e2d39d163
Red.Cardinal <boriszubar@gmail.com>
2025-06-04 21:57:23 +0300

    1

---
+++ Week2/Week2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please, select your gender (m/f):");
+
+        char userGender = Char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please, enter your last name:");
+        string userLastName = Console.ReadLine();
+
+        if (userGender == 'm')
+        {
+            Console.WriteLine($"Welcome, Mr. {userLastName}!");
+        }
+        else if (userGender == 'f')
+        {
+            Console.WriteLine($"Welcome, Ms. {userLastName}!");
+        }
+        else
+        {
+            Console.WriteLine($"Welcome, {userLastName}!");
+        }
+
+        Console.WriteLine("The program will close in 10 seconds...");
+        Thread.Sleep(10000);
+    }
+}
+
